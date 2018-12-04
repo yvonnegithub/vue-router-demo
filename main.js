c03f3196a1aca8719d91a2e511ac5c6bb6c4c7c6
yvonnegithub <13165420370@163.com>
2018-12-04 23:59:02 +0800

    model

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ var ComponentB = {
     'component-a':  ComponentA
   }
 }
+
+Vue.component('base-checkbox', {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    checked: Boolean
+  },
+  template: `
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      v-on:change="$emit('change', $event.target.checked)"
+    >
+  `
+})
